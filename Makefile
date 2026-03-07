623cffca745383b61491105edbfe69d4e2b93e0b
linux_name2 <linux2@email.org>
2026-03-08 00:01:26 +0300

    feat: add monorepo collection support (v0.6.0)

--- Makefile
+++ Makefile
@@ -7,6 +7,7 @@ install:
 	install -Dm644 completions/gitpkg.bash $(DESTDIR)$(PREFIX)/share/bash-completion/completions/gitpkg
 	install -Dm644 mirrorlist $(DESTDIR)/etc/gitpkg/mirrorlist
 	install -Dm644 pkglist $(DESTDIR)/etc/gitpkg/pkglist
+	install -Dm644 collections $(DESTDIR)/etc/gitpkg/collections
 
 uninstall:
 	rm -f $(DESTDIR)$(PREFIX)/bin/gitpkg
@@ -14,3 +15,4 @@ uninstall:
 	rm -f $(DESTDIR)$(PREFIX)/share/bash-completion/completions/gitpkg
 	rm -f $(DESTDIR)/etc/gitpkg/mirrorlist
 	rm -f $(DESTDIR)/etc/gitpkg/pkglist
+	rm -f $(DESTDIR)/etc/gitpkg/collections
--- completions/gitpkg.bash
+++ completions/gitpkg.bash
@@ -4,7 +4,7 @@ _gitpkg() {
     local cur prev words cword
     _init_completion || return
 
-    local commands="install update remove list status files inspect verify repo-add repo-del repo-list search"
+    local commands="install update remove list status files inspect verify repo-add repo-del repo-list collection-add collection-del collection-list search"
 
     local cmd="" i
     for ((i = 1; i < cword; i++)); do
@@ -40,6 +40,17 @@ _gitpkg() {
         done < /etc/gitpkg/pkglist
     fi
 
+    # Packages from cached collections
+    local subdir d name
+    for d in /var/cache/gitpkg/_collections/*/; do
+        [[ -d "$d" ]] || continue
+        for subdir in "$d"*/; do
+            [[ -f "${subdir}Makefile" ]] || continue
+            name="${subdir%/}"; name="${name##*/}"
+            known+=("$name")
+        done
+    done
+
     # Configured repo URLs
     local -a repo_urls=()
     if [[ -f /etc/gitpkg/repos.conf ]]; then
@@ -47,6 +58,16 @@ _gitpkg() {
             [[ -n "$line" && "$line" != \#* ]] && repo_urls+=("$line")
         done < /etc/gitpkg/repos.conf
     fi
+
+    # Configured collection names
+    local -a coll_names=()
+    local _cfile
+    for _cfile in /etc/gitpkg/collections.conf /etc/gitpkg/collections; do
+        [[ -f "$_cfile" ]] || continue
+        while IFS= read -r line; do
+            [[ -n "$line" && "$line" != \#* ]] && coll_names+=("$line")
+        done < "$_cfile"
+    done
 
     case "$cmd" in
         install)
@@ -92,6 +113,11 @@ _gitpkg() {
         repo-del)
             if [[ "$cur" != -* ]]; then
                 COMPREPLY=($(compgen -W "${repo_urls[*]}" -- "$cur"))
+            fi
+            ;;
+        collection-del)
+            if [[ "$cur" != -* ]]; then
+                COMPREPLY=($(compgen -W "${coll_names[*]}" -- "$cur"))
             fi
             ;;
         search)
